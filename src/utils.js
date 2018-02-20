fe5c717053efd2331d614666d8693a45a6363161
Akash Goswami <akash@fusioncharts.com>
2018-02-20 12:19:30 +0530

    Add utils

---
+++ src/utils.js
@@ -0,0 +1,11 @@
+const glob = require('glob');
+const utils = {};
+
+utils.normalizeTarget = match => {
+	if (!glob.hasMagic(match)) {
+		match = `**/${match}/**`
+	}
+	return match;
+}
+
+module.exports = utils;
