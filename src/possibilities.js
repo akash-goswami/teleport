c05ba14cfceb3949de3459d552985ae964bc6298
Akash Goswami <akash@fusioncharts.com>
2018-02-22 02:57:55 +0530

    Indent with space; sublime manage to mess up with the spacing

--- src/possibilities.js
+++ src/possibilities.js
@@ -1,14 +1,14 @@
 const service = require('./service');
 
 module.exports = async payload => {
-	if (!payload.cwd) {
-	    // If current working dir not found, raise an error
-	    throw new Error('Script not called correctly. Current working directory not found.');
-	}
-	
-	const res = await service.execute(service.search, payload);
-	const resWithStat = await service.execute(service.stats, Object.assign({ name: res }, payload));
-	return resWithStat
-	    .filter(fileStat => fileStat.isDir)
-	    .map(fileStat => fileStat.name);
+    if (!payload.cwd) {
+        // If current working dir not found, raise an error
+        throw new Error('Script not called correctly. Current working directory not found.');
+    }
+    
+    const res = await service.execute(service.search, payload);
+    const resWithStat = await service.execute(service.stats, Object.assign({ name: res }, payload));
+    return resWithStat
+        .filter(fileStat => fileStat.isDir)
+        .map(fileStat => fileStat.name);
 }
