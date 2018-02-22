c360de72b1d16028394147714f9faeb7748a8618
Akash Goswami <akash@fusioncharts.com>
2018-02-22 11:34:47 +0530

    Reader, writer and parser of configs

--- src/config.js
+++ src/config.js
@@ -11,14 +11,54 @@ const readFile = filePath => new Promise((res, rej) => {
             return rej(err);
         }
         return res(data);
-    })
-})
+    });
+});
 
-// If config file is not create create it under teleport folder
-// Two files are created tp.json and tp.hist
+const writer = {
+    CMD_HIST: data => new Promise((res, rej) => {    
+        readFile(CONFIG_PATH.CMD_HIST)
+            .then(existingData => {
+                existingData = parser.CMD_HIST(existingData.split('\n'));
+                existingData = existingData
+                    .slice(0, 49)
+                    .filter(item => item !== data);
+                existingData.unshift(data); // @todo take from config. By default 50.
+                fs.writeFile(CONFIG_PATH.CMD_HIST, existingData.join('\n'), err => {
+                    if (err) {
+                        return rej(err);
+                    }
+                    return res();
+                });        
+            })
+            .catch(e => {
+                rej(e);
+            });
+    }),
+    LINTREE_HIST: data => new Promise((res, rej) => {
+        fs.writeFile(CONFIG_PATH.LINTREE_HIST, data.map(li => li.join(' ')).join('\n'), err => {
+            if (err) {
+                return rej(err);
+            }
+            return res();
+        });
+    })
+};
+
+const parser = {
+    CMD_HIST: data => data,
+    LINTREE_HIST: data => {
+        const obj = {};
+        data.forEach(unit => obj[unit[0]] = unit[3]);
+        return obj;
+    }
+};
+
+// If config file is not created,  create it under teleport folder
+// Three files are created tp.json, tp.hist, lintree.hist
 const CONFIG_PATH = {
     EXEC: path.join(USER_DATA, LIB_NAME, 'tp.json'),
-    HIST: path.join(USER_DATA, LIB_NAME, 'tp.hist')
+    CMD_HIST: path.join(USER_DATA, LIB_NAME, 'tp.hist'),
+    LINTREE_HIST: path.join(USER_DATA, LIB_NAME, 'lintree.hist')
 };
 
 const parseConfig = async () => {
@@ -31,7 +71,31 @@ const parseConfig = async () => {
     }
 }
 
+const parseHist = async type => {
+    try {
+        let res = await readFile(CONFIG_PATH[type]);
+        res = res.split('\n').map(line => line.split(' '));
+        return parser[type](res);
+    } catch (e) {
+        throw new Error('Error: Can\'t read config file.');
+    }
+}
+
+const writeHist = async (type, data) => {
+    try {
+        await writer[type](data);
+    } catch (e) {
+        throw new Error('Error: Can\'t write to config file.');
+    }
+}
+
 module.exports = {
+    type: {
+        CMD_HIST: 'CMD_HIST',
+        LINTREE_HIST: 'LINTREE_HIST'
+    },
     CONFIG_PATH,
-    parseConfig
+    parseConfig,
+    parseHist,
+    writeHist
 }
