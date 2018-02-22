86b0cf2759ca778a98f94b357ca98eb92b674f0c
Akash Goswami <akash@fusioncharts.com>
2018-02-22 11:39:25 +0530

    Correct the damn sublime indentation

--- src/utils.js
+++ src/utils.js
@@ -2,10 +2,10 @@ const glob = require('glob');
 const utils = {};
 
 utils.normalizeTarget = match => {
-	if (!glob.hasMagic(match)) {
-		match = `**/${match}/**`
-	}
-	return match;
+    if (!glob.hasMagic(match)) {
+        match = `**/${match}/**`
+    }
+    return match;
 }
 
 module.exports = utils;
